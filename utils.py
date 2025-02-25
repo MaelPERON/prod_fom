b0b4183e1f8bc2b79a231e3e869d8db4f5c4d418
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-02-25 15:29:11 +0100

    Add multiple utils functions
    get_collection
    get_objs
    set_mesh_name
    is_asset
    obj_to_json

--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
 import bpy
+
+def get_collection(string):
+    list = [coll for coll in bpy.data.collections if string in coll.name]
+    return None if len(list) < 1 else list[0]
+
+def get_objs(objs):
+    return [obj for obj in objs if obj.type == "MESH"]
+
+def set_mesh_name(obj):
+    obj.data.name = obj.name
+
+def is_asset(filepath):
+    return len(filepath.split("02_PROD\\assets")) >= 1
+
+def obj_to_json(obj):
+    new_obj = {}
+    for attr in dir(obj):
+        if attr.startswith("__") or attr.startswith("bl_"): continue
+        value = getattr(obj, attr, None)
+        id_data = getattr(value, "id_data", None)
+        # print(attr, value, id_data)
+        new_obj[attr] = value if id_data is None else None
+    return new_obj
